1fcbc68cd1d9308192e4849ec9458c055459be68
Johan Nordström <johan@digitalvillage.se>
2026-09-23 19:42:31 +0200

    Our very first React "app" with a stateless variable and a `onClick`-event.

--- 01-react-basics/src/App.tsx
+++ 01-react-basics/src/App.tsx
@@ -1,7 +1,28 @@
 import "./App.css";
 
 function App() {
-	return <h1>01-react-basics</h1>;
+	let counter = 0;
+
+	const handleBtnClick = () => {
+		console.log("Stop it, that tickles! 😂");
+
+		console.log("Counter before increase:", counter);
+
+		// increase counter
+		counter++;
+
+		console.log("Counter after increase:", counter);
+	}
+
+	return (
+		<>
+			<h1>01-react-basics</h1>
+
+			<p>Counter: {counter}</p>
+
+			<button onClick={handleBtnClick}>Click me!</button>
+		</>
+	);
 }
 
 export default App;
