18f19af678cccb231251b1041820e6cb5e772b27
Johan Nordström <johan@digitalvillage.se>
2026-09-23 20:59:48 +0200

    Added exercise for lesson 2.

--- 01-react-basics/src/App.tsx
+++ 01-react-basics/src/App.tsx
@@ -46,6 +46,40 @@ function App() {
 
 			<hr />
 
+			<h2>Salary</h2>
+
+			<p>Salary per hour: SALARY &euro;</p>
+
+			<div className="buttons">
+				<div className="mb-1">
+					<button
+						className="btn btn-primary btn-lg"
+					>
+						Raise 1 &euro; 🤑
+					</button>
+					<button
+						className="btn btn-warning btn-lg"
+					>
+						Decrease 1 &euro; 😢
+					</button>
+				</div>
+
+				<div className="mb-1">
+					<button
+						className="btn btn-primary btn-lg"
+					>
+						Raise 5 &euro; 🤑🤑🤑
+					</button>
+					<button
+						className="btn btn-warning btn-lg"
+					>
+						Decrease 5 &euro; 😢😢😢
+					</button>
+				</div>
+			</div>
+
+			<hr />
+
 			<h2>Posts</h2>
 			<ul>
 				{posts.map(post =>
---
+++ exercise-for-lesson-2.md
@@ -0,0 +1,13 @@
+# Övning inför lektion 2
+
+Start-timlön ska vara €10.
+
+1. Lägg till funktionalitet för att öka och minska timlönen med €1.
+
+2. Lägg till funktionalitet för att öka och minska timlönen med €5.
+
+3. 🌟 Timlönen ska ej kunna bli mindre än €5.
+
+4. 🚀 Visa ett meddelande om timlönen går under €10 (men inte annars).
+
+5. Förbättra ökning och minskning av timlönen så att det går att öka och minska med valfri summa (positiv eller negativ) som användaren anger.
